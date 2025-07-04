f086e869ed2485a3c80dda7a569f7cd1479e721a
Neil J George <neilgeorge1509@gmail.com>
2025-07-05 00:14:58 +0530

    home file

--- lootify/Lootify/views.py
+++ lootify/Lootify/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("✅ Django is working — lootify → Lootify") 
+    return render(request, "Lootify/home.html") 
